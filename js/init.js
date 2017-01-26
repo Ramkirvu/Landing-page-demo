9162757abff6a8318bbb67a5ef63cff3979d7462
Ramkirvu <Ramkirvu@gmail.com>
2017-01-26 21:37:19 +0300

    First

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+})(jQuery); // end of jQuery name space
