f8d6905ab015c7a2cd86d971db278c203f139eba
Konstantin <Ramkirvu@gmail.com>
2017-01-28 17:00:54 +0300

    Landing page «Wood site» v1.2

--- js/init.js
+++ js/init.js
@@ -5,7 +5,16 @@
     $('.parallax').parallax();
 
   }); // end of document ready
-    $(document).ready(function(){
-      $('.carousel').carousel();
-    });
+  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+		 e.preventDefault();
+		 
+		var target = this.hash,
+		 $target = $(target);
+		 
+		$('html, body').stop().animate({
+		 'scrollTop': $target.offset().top
+		 }, 500, 'swing', function () {
+		 window.location.hash = target;
+	 });
+ });
 })(jQuery); // end of jQuery name space
